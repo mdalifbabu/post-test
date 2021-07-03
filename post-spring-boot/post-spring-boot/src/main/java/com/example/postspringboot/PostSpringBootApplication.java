9b81fb2b88cdb233f2701e4540a1a771985cd484
Azizul Haque Alif <55746705+azizulhaquealif@users.noreply.github.com>
2021-07-04 03:06:00 +0600

    final commit

---
+++ post-spring-boot/post-spring-boot/src/main/java/com/example/postspringboot/PostSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.postspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostSpringBootApplication.class, args);
+	}
+
+}
---
+++ post-spring-boot/post-spring-boot/src/test/java/com/example/postspringboot/PostSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
